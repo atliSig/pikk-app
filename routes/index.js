cede14d4db9590f4df817efbea8133d60b319320
Atli <atli@ja.is>
2016-10-21 22:02:23 +0000

    added routing to place view

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/search',function(req,res,next){
   res.render('resultlist',{title:'now I am here'});
 });
 
+router.get('/place',function(req,res,next){
+  res.render('place',{title:'And now I am here :O'});
+});
+
 module.exports = router;
