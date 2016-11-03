0c7710ba82fa39a379862fd99e25cf6bdd6e423f
Atli <atli@ja.is>
2016-11-03 22:36:43 +0000

    some old changes

--- middleware/apiTools.js
+++ middleware/apiTools.js
@@ -4,14 +4,32 @@
 var https = require('https');
 var url = require('url');
 
-urlPrepper = function(theUrl){
-    var query = encodeURI(url.parse(theUrl,true).query.query); //wtf tho
+urlPrepper = function(theUrl,type){
+    var query;
+    if(type){
+        query = encodeURI(theUrl.substr(7));
+        console.log(query);
+    }else{
+        query = encodeURI(url.parse(theUrl,true).query.query);
+    }
     return "https://api.ja.is/search?q="+query+"&access_code="+process.env.SEARCH_KEY;
 };
 
+exports.showPlace = function(req,res,next){
+    var str='';
+    https.get(urlPrepper(req.url,1),function(response) {
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+        response.on('end', function () {
+            req.place_result = JSON.parse(str).yellow.items[0] // only one result;
+            next();
+        })
+    });
+}
 exports.callAPI = function(req,res,next){
     var str='';
-    https.get(urlPrepper(req.url),function(response) {
+    https.get(urlPrepper(req.url,0),function(response) {
         response.on('data', function (chunk) {
             str += chunk;
         });
--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,12 @@ function getSearch(req,res,next){
     res.render('resultlist',{results: req.search_result});
 }
 
-router.get('/place',function(req,res,next) {
-  res.render('place', {title: 'And now I am here :O'});
-});
+router.get('/place/*',apiTools.showPlace,getPlace);
+
+function getPlace(req,res,next) {
+  console.log(req.place_result);
+  res.render('place',{place: req.place_result});
+}
 
 router.get('/headout', function(req, res, next) {
   var pikk = pikkJson.yellow.items[0];
