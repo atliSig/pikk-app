cf3a12cfbec5432ad2eedd4bc79513ef52c09722
Matthías Karl Karlsson <Matthas Karl Karlsson>
2016-11-06 23:50:02 +0000

    Worked on the look for /place

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,7 @@ router.get('/', function(req, res, next) {
 router.get('/search', apiTools.doSearch, getSearch);
 
 function getSearch(req,res,next){
+    console.log(req.search_result)
     res.render('resultlist',{results: req.search_result});
 }
 
