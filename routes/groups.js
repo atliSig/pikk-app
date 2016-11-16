1f779545e3798d11d73d14a18f9d6e97a87146f3
Petur Helgi Einarsson <phe1@hi.is>
2016-11-16 20:56:01 +0000

    Changes to layout and groups

--- routes/groups.js
+++ routes/groups.js
@@ -9,19 +9,20 @@ var groups = require('../lib/groups');
 
 module.exports = router;
 
-router.get('/', auth.ensure_logged_in, display_group_page);
+router.get('/', auth.ensure_logged_in, displayGroupPage);
+router.post('/creategroup', auth.ensure_logged_in, createGroup)
 
 
 
 
 //\\\\\\\\\\functions//////////\\
 
-function display_group_page(req, res, next){
+function displayGroupPage(req, res, next){
     if(req.session)
         var user = req.session.user;
     else var user = JSON.parse(process.env.user);
-    console.log(user);
-    groups.get_groups(user.id, function(err, all){
+
+    groups.getGroups(user.id, function(err, all){
         if(!err){
             if(all.length == 0)
                 all = false
@@ -35,5 +36,18 @@ function display_group_page(req, res, next){
             console.log(err);
             res.redirect('/');
         }
+    });
+}
+
+function createGroup(req, res, next){
+    if(req.session)
+        var user = req.session.user;
+    else var user = JSON.parse(process.env.user);
+    var members = req.body.members;
+    var groupName = req.body.groupname;
+    var description = req.body.description;
+    console.log(req.url);
+    groups.createGroup(user, members, groupName, description, function(err, all){
+        res.render('groups');
     });
 }
