fc50ab0211bcdc55b561026aa1409df9818a016e
Atli <atli@ja.is>
2016-11-21 04:03:02 +0000

    Fixed footer

--- public/javascript/slide.js
+++ public/javascript/slide.js
@@ -12,6 +12,31 @@ $(document).ready(function() {
             slidesToScroll: 1,
             autoplay: true,
             autoplaySpeed: 4000,
+            responsive: [
+                {
+                    breakpoint: 1170,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 940,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         }
     )
 });
