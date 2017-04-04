85c73b7e5f5cd3ab2340eed2227009967c54c269
Matthías Karl Karlsson <mattik15@gmail.com>
2017-04-04 21:47:23 +0000

    tag search in index

--- routes/api/index.js
+++ routes/api/index.js
@@ -126,34 +126,180 @@ router.get('/tags',function(req,res){
     var tags = [
         {
             id: 1,
-            ice_name: 'kjúklingur',
-            en_name: 'chicken',
+            tag1: 'kjúklingur',
+            name: 'Chicken',
+            tag2: 'chicken',
             desc: 'Chicken is amazing and healthy!',
             imgurl:  'drasl'
         },
         {
-            id:
+            id: 2,
+            tag1: 'steik',
+            name: 'Steak',
+            tag2: 'beef',
+            desc: 'Where\'s the beef?',
+            imgurl: 'drasl'
+        },
+        {
+            id: 3,
+            tag1: 'pítsa',
+            name: 'Pizza',
+            tag2: 'pizza',
+            desc: 'No one can say no to a pizza?',
+            imgurl: 'drasl'
+        },
+        {
+            id: 4,
+            tag1: 'hamborgari',
+            name: "Hamburger",
+            tag2: 'burger',
+            desc: 'A hamburger is a good foody?',
+            imgurl: 'drasl'
+        },
+        {
+            id: 5,
+            tag1: 'sjávarréttir',
+            name: 'Seafood',
+            tag2: 'seafood',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 6,
+            tag1: 'sushi',
+            name: 'Sushi',
+            tag2: 'healthy',
+            desc: '123',
+            imgurl: 'drasl'
+        },
+        {
+            id: 7,
+            tag1: 'salat',
+            name: 'Salat',
+            tag2: 'healthy',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 7,
+            tag1: 'arabískt',
+            name: 'Kebab',
+            tag2: 'kebab',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 8,
+            tag1: 'tælenskt',
+            name: 'Thai',
+            tag2: 'thai',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 9,
+            tag1: 'asískt',
+            name: 'Asian',
+            tag2: 'asian',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 10,
+            tag1: 'indverskt',
+            name: 'Indian',
+            tag2: 'indian',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 11,
+            tag1: 'mexikóskt',
+            name: 'Mexican',
+            tag2: 'mexican',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 12,
+            tag1: 'tælenskt',
+            name: 'Thai',
+            tag2: 'thai',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 13,
+            tag1: 'ítalskt',
+            name: 'Italian',
+            tag2: 'italian',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 14,
+            tag1: 'íslenskt',
+            name: 'Icelandic',
+            tag2: 'icelandic',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 13,
+            tag1: 'hádegismatur',
+            name: 'Brunch',
+            tag2: 'brunch',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 14,
+            tag1: 'vegan',
+            name: 'Vegan',
+            tag2: 'healthy',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 15,
+            tag1: 'hollt',
+            name: 'Hollt',
+            tag2: 'healthy',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 14,
+            tag1: 'bakarí',
+            name: 'Bakery',
+            tag2: 'bakery',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 15,
+            tag1: 'bístró',
+            name: 'Bistro',
+            tag2: 'bistro',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 16,
+            tag1: 'kaffi',
+            name: 'Coffee',
+            tag2: 'juice',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
+        },
+        {
+            id: 17,
+            tag1: 'áfengi',
+            name: 'Alcohol',
+            tag2: 'alcohol',
+            desc: 'Under the sea. Under the sea!',
+            imgurl: 'drasl'
         }
-        ['steik', 'beef'],
-        ['sjávarréttir', 'seafood'],
-        ['pítsa', 'pizza'],
-        ['hamborgari', 'burger'],
-        ['sushi', 'healthy'],
-        ['salat', 'healthy'],
-        ['kebab', 'arabískt'],
-        ['tælenskt', 'thai'],
-        ['asískt', 'asian'],
-        ['indverskt', 'indian'],
-        ['mexíkóskt', 'mexican'],
-        ['ítalskt', 'italian'],
-        ['íslenskt', 'icelandic'],
-        ['hádegismatur', 'brunch'],
-        ['vegan', 'healthy'],
-        ['hollt', 'healthy'],
-        ['bakarí', 'bakery'],
-        ['bístró', 'bistro'],
-        ['kaffi', 'juice'],
-        ['áfengi', 'alcohol']
     ];
     res.send({
         tags: tags
